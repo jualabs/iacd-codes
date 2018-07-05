2e28724cbf17366c73ab4670567a04a8f5d06d1f
victorwcm <victorwcm@gmail.com>
2018-07-05 16:38:19 -0300

    Add files via upload

---
+++ wemos-tb-mqtt.ino
@@ -0,0 +1,125 @@
+/* inclusao das bibliotecas */
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <TimeAlarms.h>
+#include <DHT.h>
+
+/* ssid e password do AP ao qual se deseja conectar */
+const char* ssid = "ssid";
+const char* password = "password";
+/* endereco do servidor onde está instalado o ThingsBoard
+// const char* mqtt_server = "192.168.X.XXX";
+/* ou para o ThingsBoard live demo */
+const char* mqtt_server = "demo.thingsboard.io";
+/* credenciais para o device que se deseja conectar */
+const char* device_token = "device_token";
+/* definicoes para o sensor DHT-22 */
+#define DHTPIN D3
+#define DHTTYPE DHT22
+
+/* instancia o cliente Wi-Fi */
+WiFiClient wifi_client;
+/* instancia o cliente MQTT */
+PubSubClient mqtt_client(wifi_client);
+/* instancia o sensor DHT */
+DHT dht(DHTPIN, DHTTYPE);
+/* variavel que guarda o timestamp do ultimo envio */
+uint32_t ultimo_envio = 0;
+
+void conectar_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("conectando a ");
+  Serial.println(ssid);
+  /* tenta iniciar a conexao com a rede Wi-Fi */
+  WiFi.begin(ssid, password);
+  /* verifica o status e aguarda a conexao */
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  /* imprime na serial IP da conexao Wi-Fi */
+  Serial.println("");
+  Serial.println("conectado ao AP");
+  Serial.println("endereco IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconectar() {
+  /* loop ate que esteja reconectado com o broker MQTT */
+  while (!mqtt_client.connected()) {
+    /* verifica a conexao Wi-Fi */
+    if (WiFi.status() != WL_CONNECTED) {
+      /* reconecta com o Wi-Fi */
+      conectar_wifi();
+    }
+    Serial.print("tentando conexao com o ThingsBoard via MQTT...");
+    /* tentativa de conexao */
+    if (mqtt_client.connect("wemos", device_token, NULL)) {
+      Serial.println("conectado");
+    } 
+    else {
+      Serial.print("falhou, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" tentando novamente em 5s");
+      /* tenta novamente daqui a 5s */
+      delay(5000);
+    }
+  }
+}
+
+void coletar_e_enviar_dados() {
+  Serial.print("coletando dados de temperatura e umidade... ");
+  /* realiza a leitura da umidade (a leitura leva 250 ms) */
+  float h = dht.readHumidity();
+  /* realiza a leitura da temperatura (a leitura leva 250 ms) */
+  float t = dht.readTemperature();
+  /* verifica se as leituras foram corretas, caso contrario retorna da funcao para tentar novamente */
+  if (isnan(h) || isnan(t)) {
+    Serial.println("falha na leitura do sensor DHT!");
+    return;
+  }
+  /* imprime na serial os valores lidos dos sensores */
+  Serial.print("umidade: ");
+  Serial.print(h);
+  Serial.print(" % | temperatura: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  /* converte os dados lidos para String */
+  String temperatura = String(t);
+  String umidade = String(h);
+  /* cria o payload da mensagem MQTT no formato JSON */
+  String payload = "{";
+  payload += "\"temperatura\":"; payload += temperatura; payload += ",";
+  payload += "\"umidade\":"; payload += umidade;
+  payload += "}";
+  /* converte os dados em formato JSON de String para char[] */
+  char dados_json[100];
+  payload.toCharArray(dados_json, 100);
+  /* envia o pacote MQTT com os dados dos sensores */
+  mqtt_client.publish( "v1/devices/me/telemetry", dados_json);
+}
+
+void setup() {
+  /* configura a interface serial */
+  Serial.begin(115200);
+  /* configura o sensor DHT */
+  dht.begin();
+  /* conectar a rede Wi-Fi */
+  conectar_wifi();
+  /* conectar ao broker MQTT */
+  mqtt_client.setServer(mqtt_server, 1883);
+  /* programa um evento de tempo a cada 10s para enviar os dados dos sensores */
+  Alarm.timerRepeat(10, coletar_e_enviar_dados);
+}
+
+void loop() {
+  /* verifica status da conexao */
+  if (!mqtt_client.connected()) {
+    reconectar();
+  }
+  /* executa a funcao de loop da biblioteca TimeAlarm */
+  Alarm.delay(100);
+  /* executa a funcao de loop do cliente MQTT */
+  mqtt_client.loop();
+}
